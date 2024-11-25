7ac7fbde55e31d2c7bc4f1ab062f92e18e30482b
Franco Carrara <frankko212@gmail.com>
2024-11-25 19:11:44 -0300

    Add product page details

--- clase-1/next.config.ts
+++ clase-1/next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [{
+      hostname: "fakestoreapi.com"
+    }]
+  }
   /* config options here */
 };
 
