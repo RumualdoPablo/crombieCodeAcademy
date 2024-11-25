3a9e523d05e9913d431988eb38cc20b021b62eea
gerogrimaldi <gerogirmaldi2@gmail.com>
2024-11-25 18:38:09 -0300

    añado imagenes stock

---
+++ clase-1/app/about/page.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+            <h2 className="text-3xl font-bold text-center mb-8">Nuestra empresa</h2>
+        <img
+                src="https://media.istockphoto.com/id/499785795/photo/analyzing-electronic-document.jpg?s=612x612&w=0&k=20&c=PYrD8albkJQHy_sgjIFCEScjfe-4DTylouvEL-oHfOU="
+                alt="Nuestra empresa"
+                className="h-auto max-w-full"
+        />
+        <img
+                src="https://media.istockphoto.com/id/1349030917/es/foto/negocios-y-finanzas-mirando-hacia-los-edificios-de-oficinas-de-gran-altura-en-el-distrito.jpg?s=612x612&w=0&k=20&c=_L5sYNxsZaMlEpbT00mwuAfWKps5MqLoCBTLMukFgF0="
+                alt="Nuestra empresa"
+                className="h-auto max-w-full"
+        />
+        <img
+                src="https://media.istockphoto.com/id/1365436662/es/foto/asociaci%C3%B3n-de-%C3%A9xito.jpg?s=612x612&w=0&k=20&c=FncLRnhPerwS05mDOuA5jFVDDqBapb4Y0DZjzUMD_gk="
+                alt="Nuestra empresa"
+                className="h-auto max-w-full"
+        />
+        </div>
+    </div>
+    
+  )
+}
+
+export default page
