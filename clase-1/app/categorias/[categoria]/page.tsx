1c7122afadfc8e32a1e780646e5bd4a799e1297b
LucasJavier <lucasbonacci@hotmail.com>
2024-11-25 19:06:04 -0300

    imagenes de productos con sus links

--- clase-1/app/categorias/[categoria]/page.tsx
+++ clase-1/app/categorias/[categoria]/page.tsx
@@ -1,4 +1,41 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+const imageArray = [
+    <Image
+    className="dark:invert"
+    src="/images/zapatillas.png"
+    alt="zapatillas"
+    width={180}
+    height={38}
+    priority
+  />,
+    <Image
+    className="dark:invert"
+    src="/images/camisas.png"
+    alt="camisas"
+    width={180}
+    height={38}
+    priority
+  />,
+    <Image
+    className="dark:invert"
+    src="/images/jeans.png"
+    alt="jean"
+    width={180}
+    height={38}
+    priority
+  />,
+    <Image
+    className="dark:invert"
+    src="/images/remera.jpg"
+    alt="remera"
+    width={180}
+    height={38}
+    priority
+  />
+]
 
 export default async function page({
   params
@@ -7,11 +44,34 @@ export default async function page({
 }) {
 
   const categoria = (await params).categoria
-
+  let indexImage = 0;
+  let nombreProducto ="";
+  switch(categoria){
+    case "women": 
+      indexImage = 0;
+      nombreProducto = "zapatilla";
+      break;
+    case "men": 
+    indexImage = 1;
+    nombreProducto = "camisa";
+    break;
+    case "kids": 
+    indexImage = 2;
+    nombreProducto = "jean";
+    break;
+    case "outlet": 
+    indexImage = 3;
+    nombreProducto = "remera";
+    break;
+  }
 
   return (
     <>
       <h1>categoria {categoria}</h1>
-      <div></div>
+      <div >
+        <div>
+        <Link href={`/producto/${nombreProducto}`}> {imageArray[indexImage]}</Link>
+        </div>
+      </div>
     </>);
 }
