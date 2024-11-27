4cf18f4464359a1622400616c19651e2486bbcda
Franco Carrara <frankko212@gmail.com>
2024-11-27 18:02:14 -0300

    Add page to list all products

--- clase-1/app/producto/[...productId]/page.tsx
+++ clase-1/app/producto/[...productId]/page.tsx
@@ -21,25 +21,32 @@ export default async function Product({
     const product: ProductTypes = await data.json();
 
     return (
-        <main className="flex flex-row gap-12 p-32">
+        <main className="flex flex-col md:flex-row gap-12 p-8 md:p-32 items-center md:items-start justify-center">
             <Image
                 src={product.image}
                 width={250}
                 height={250}
                 alt={`image of ${product.title}`}
             />
-            <div className="flex flex-col gap-4">
-                <h1 className="text-5xl">{product.title}</h1>
-                <p>{product.description}</p>
+            <div className="flex flex-col gap-4 items-center md:items-start">
+                <h1 className="text-3xl md:text-5xl text-center md:text-left">
+                    {product.title}
+                </h1>
+                <p className="text-center md:text-left">
+                    {product.description}
+                </p>
                 <span className="text-2xl">
                     {new Intl.NumberFormat("en-US", {
                         style: "currency",
                         currency: "ARS",
                     }).format(product.price)}
                 </span>
-                <button className="dark:bg-white bg-black p-4 text-white dark:text-black rounded-md hover:opacity-80 hover:cursor-pointer justify-self-end mx-0">
-                    Comprar ahora
-                </button>
+
+                <div className="w-full md:w-64 mt-auto">
+                    <button className="dark:bg-white bg-black p-4 text-white dark:text-black rounded-md hover:opacity-80 hover:cursor-pointer w-full">
+                        Comprar ahora
+                    </button>
+                </div>
             </div>
         </main>
     );
