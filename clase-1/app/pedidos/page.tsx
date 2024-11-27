7230a35b72a13d72a419f8872a3698ab38783c1c
gerogrimaldi <gerogirmaldi2@gmail.com>
2024-11-27 19:23:23 -0300

    - se quitaron variables sin uso - se agrego funcionalidad para que el color de la respuesta cambie segun el estado del pedido (verde: entregado, rojo: cancelado, gris: en viaje)

--- clase-1/app/pedidos/page.tsx
+++ clase-1/app/pedidos/page.tsx
@@ -2,18 +2,14 @@
 import React from 'react'
 import { useState } from 'react';
 import pedidos from '../../db/pedidos.ts';
-import { stat } from 'fs';
 
 const page = () => {
 
 const [numPedido, setNumPedido] = useState("");
 const [pedidoEstado, setPedidoEstado] = useState<string | null>(null); // Estado para mostrar el estado del pedido
-const btn = document.getElementById('button')
 
 const checkPedidoStatus = () => {
     const pedido = pedidos.find(pedido => pedido.numeroPedido === Number(numPedido)) || null;
-
-    let status = "Pedido no encontrado";
 
     if (pedido) {
         setPedidoEstado(pedido.estado);
@@ -32,20 +28,23 @@ const checkPedidoStatus = () => {
                 <h1 className="text-2xl font-bold text-center mb-6">Averiguar estado de tu pedido</h1>
                 <div className="space-y-4">
                     <input
-                    type="text"
-                    placeholder="Numero de pedido"
-                    onChange={ (e) => setNumPedido(e.target.value)}
-                    className="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-950 text-black"
+                        type="text"
+                        placeholder="Numero de pedido (100X)"
+                        onChange={ (e) => setNumPedido(e.target.value)}
+                        className="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-950 text-black"
                     />
                     <button
-                    id="button"
-                    onClick={checkPedidoStatus}
-                    className="w-full bg-green-600 text-white py-2 rounded-lg font-semibold shadow-md hover:bg-green-700 transition duration-300">
+                        id="button"
+                        onClick={checkPedidoStatus}
+                        className="w-full bg-green-600 text-white py-2 rounded-lg font-semibold shadow-md hover:bg-green-700 transition duration-300"
+                    >
                     Consultar
                     </button>
-                    {/* Mostrar el estado del pedido debajo del botón */}
                     {pedidoEstado && (
-                    <h2 className="text-center mt-4 text-lg font-semibold">
+                    <h2 
+                    className={` ${pedidoEstado === "entregado" ? 'bg-green-500' : //color condicional segun estado pedido
+                        pedidoEstado === "cancelado" ? 'bg-red-500' : 'bg-gray-500'}
+                    text-center mt-4 text-lg font-semibold`}>
                         Estado: {pedidoEstado}
                     </h2>
                     )}
