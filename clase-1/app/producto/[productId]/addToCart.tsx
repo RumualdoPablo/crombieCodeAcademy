fe4ec6a46cfca15b38d0445d247d96ef3d2359ea
Franco Carrara <frankko212@gmail.com>
2024-12-02 15:26:34 -0300

    add toast to inform when a product is added to cart

--- clase-1/app/producto/[productId]/addToCart.tsx
+++ clase-1/app/producto/[productId]/addToCart.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import Link from "next/link";
+import toast, { Toaster } from "react-hot-toast";
+
 export function addToCart(productId: string | { productId: string }) {
     const id = typeof productId === "string" ? productId : productId.productId;
 
@@ -11,15 +14,19 @@ export function addToCart(productId: string | { productId: string }) {
         cart[id] = cart[id] ? cart[id] + 1 : 1;
         window.localStorage.setItem("carrito", JSON.stringify(cart));
     }
+    toast.success("Producto agregado al carrito", {position: "bottom-right"});
 }
 
 export function AddToCartButton(productId: string | { productId: string }) {
     return (
-        <button
-            className="dark:bg-white bg-black p-4 text-white dark:text-black rounded-md hover:opacity-80 hover:cursor-pointer w-full"
-            onClick={() => addToCart(productId)}
-        >
-            Comprar ahora
-        </button>
+        <>
+            <button
+                className="dark:bg-white bg-black p-4 text-white dark:text-black rounded-md hover:opacity-80 hover:cursor-pointer w-full"
+                onClick={() => addToCart(productId)}
+                >
+                Comprar ahora
+            </button>
+            <Toaster/>
+        </>
     );
 }
